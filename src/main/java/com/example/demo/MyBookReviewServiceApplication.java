c3d1ad3cf5374fbe3301414dad8c41ca3e399277
Avinash <avinash.mait@gmail.com>
2017-06-25 12:03:26 +0530

    initial commit

---
+++ src/main/java/com/example/demo/MyBookReviewServiceApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RequestMapping("/book-reviews")
+public class MyBookReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBookReviewServiceApplication.class, args);
+    }
+
+    @GetMapping("/available")
+    public String isAvailable() {
+        return "My Book Review Service is available";
+    }
+
+}
