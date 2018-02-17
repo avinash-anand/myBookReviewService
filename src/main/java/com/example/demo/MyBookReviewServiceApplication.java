4515d89f8b23cccebf839888a494517b8cb990aa
Avinash <avinash.mait@gmail.com>
2018-02-17 11:00:34 +0530

    adding zipkin - for tracing

--- src/main/java/com/example/demo/MyBookReviewServiceApplication.java
+++ src/main/java/com/example/demo/MyBookReviewServiceApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,11 @@ public class MyBookReviewServiceApplication {
     @GetMapping("/available")
     public String isAvailable() {
         return "My Book Review Service is available";
+    }
+    
+    @Bean
+    public AlwaysSampler alwaysSampler() {
+        return new AlwaysSampler();
     }
 
 }
